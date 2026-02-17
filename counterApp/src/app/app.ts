57e01412e8d20e6fedc27a53294e55f4ca31f4bc
sesingh <sesingh@ciena.com>
2026-02-17 17:18:23 +0530

    set and get function

--- counterApp/src/app/app.ts
+++ counterApp/src/app/app.ts
@@ -3,21 +3,43 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  // protected readonly title = signal('angular-prac');
-  count=0
 
-  handleCounter(val:string){
-    if(val=='minus') {
-      this.count = this.count - 1;
-    }else if(val=='plus'){
-      this.count=this.count+1;
-    }else if(val=='reset'){
-      this.count=0;
-    }
+  name=""
+  displayName=""
+
+  getName(event:Event) {
+    this.name = (event.target as HTMLInputElement).value
+  }
+  showName() {
+    this.displayName = this.name
+  }
+
+  // protected readonly title = signal('angular-prac');
+  // count=0
+  //
+  // handleCounter(val:string){
+  //   if(val=='minus') {
+  //     this.count = this.count - 1;
+  //   }else if(val=='plus'){
+  //     this.count=this.count+1;
+  //   }else if(val=='reset'){
+  //     this.count=0;
+  //   }
+  email: string="";
+  setName() {
+    this.displayName ="sahil"
+  }
+
+  getEmail(value: string) {
+    this.email=value
+  }
+
+  setEmail() {
+    this.email="def@test.com"
   }
 }
